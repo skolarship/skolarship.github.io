b1cff588b36396e5ca8b91851a1a153fb8017c68
Chafic Nehme <cnehm006@uottawa.ca>
2023-10-23 21:52:00 -0400

    Added about.html and fixed the navbar. - Used Flexbox to align the navigation bar links to the right - Added styles for every modification needed - Added a picture carousel in about.html - Fixed the carousel sliding behavior for smoother transitions - Resolved the overlay issue where team members were stacking on top of each other - Corrected the directionality of the left and right navigation arrows - Enhanced mobile responsiveness with viewport adjustments - Updated CSS for better visual presentation and layout stability

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-var a = 1;
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelectorAll('.team-member');
+    const slideWidth = slides[0].offsetWidth;
+
+    // Adjust the current slide pointer
+    currentSlide += direction;
+
+    // Ensure currentSlide is within the boundaries
+    if (currentSlide < 0) {
+        currentSlide = 0;
+    } else if (currentSlide > slides.length - 1) {
+        currentSlide = slides.length - 1;
+    }
+    
+    const slideAmount = -currentSlide * slideWidth;
+    document.querySelector('.slides-wrapper').style.transform = `translateX(${slideAmount}px)`;
+}
+
+// Initial setup to show the first slide
+document.addEventListener("DOMContentLoaded", function() {
+    moveSlide(0); // This initializes the slider to show the first slide
+});
