6a351af60bf7d4937caa209c5d806b50dbad590a
Chafic Nehme <cnehm006@uottawa.ca>
2023-11-25 20:23:15 -0500

    MASSIVE OVERHAUL -Mobile view now working -CSS FILE FOR (almost) EVERY HTML FILE -Sleeker look on all pages -Removed wave and static background (Amy pls fix) -Revamped carousel

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-let currentSlide = 0;
-
-function moveSlide(direction) {
-    const slides = document.querySelectorAll('.team-member');
-    const slideWidth = slides[0].offsetWidth;
-
-    // Adjust the current slide pointer
-    currentSlide += direction;
-
-    // Ensure currentSlide is within the boundaries
-    if (currentSlide < 0) {
-        currentSlide = 0;
-    } else if (currentSlide > slides.length - 1) {
-        currentSlide = slides.length - 1;
-    }
-    
-    const slideAmount = -currentSlide * slideWidth;
-    document.querySelector('.slides-wrapper').style.transform = `translateX(${slideAmount}px)`;
-}
-
-// Initial setup to show the first slide
-document.addEventListener("DOMContentLoaded", function() {
-    moveSlide(0); // This initializes the slider to show the first slide
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navbarLinks = document.querySelector(".mobile-navbar-links");
+  
+    hamburger.addEventListener("click", function () {
+      if (navbarLinks.style.display === "block") {
+        navbarLinks.style.display = "none";
+      } else {
+        navbarLinks.style.display = "block";
+      }
+    });
+  });
